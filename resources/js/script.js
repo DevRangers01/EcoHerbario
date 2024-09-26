afa1a01432aef997f833289447f73fa73a007c0d
Aluno <teteoliveira876@gmail.com>
2024-09-26 14:25:29 -0300

    foi ae

---
+++ resources/js/script.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function() {
+        document.querySelectorAll('nav ul li a').forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
--- routes/web.php
+++ routes/web.php
@@ -5,32 +5,12 @@ use App\Http\Controllers\GrassController;
 use App\Http\Controllers\UsuarioController;
 
 Route::get('/GrassController', [\App\Http\Controllers\GrassController::class, '__construct']);
-$host = 'localhost';
-$user = 'usuario';
-$password = 'senha';
-$dbname = 'nome_do_banco';
-
-$connection = mysqli_connect($host, $user, $password, $dbname);
-
-if (!$connection) {
-    die("Erro: " . mysqli_connect_error());
-}
-
-$grassController = new GrassController($connection);
-
+Route::get('/especies', [GrassController::class, 'getAllEspecies']);
+Route::get('/especies/{id}', [GrassController::class, 'getEspecieById']);
+Route::post('/especies', [GrassController::class, 'createEspecie']);
+Route::delete('/especies/{id}', [GrassController::class, 'deleteEspecie']);
 
 Route::post('/register', [UsuarioController::class, 'createUser']);
-$usuarioController->createUser('usuario', 'senha', 'email@example.com');
-
 Route::post('/login', [UsuarioController::class, 'login']);
-$loginStatus = $usuarioController->login('usuario', 'senha');
-if ($loginStatus) {
-    echo "Bem vindo";
-} else {
-    echo "Falha no login";
-}
-
 Route::post('/logout', [UsuarioController::class, 'logout']);
-$usuarioController->logout();
-echo "Você saiu com sucesso!";
 
